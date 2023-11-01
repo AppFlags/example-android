0484ccdf1cc4c1b682dcd1fa3221ccf3fb42e924
Justin Capogna <jcapogna@gmail.com>
2023-10-31 19:45:18 -0500

    new example app

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AppFlags Android Kotlin Example"
+include(":app")
+ 
