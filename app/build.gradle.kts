1d1c32fa326a232d226dac5ba24627a6e5f10391
Justin Capogna <jcapogna@gmail.com>
2024-01-01 15:38:42 -0600

    Upgrade to appflags-sdk-android 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-    implementation("io.appflags:appflags-sdk-android:1.0.1")
+    implementation("io.appflags:appflags-sdk-android:1.0.2")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
